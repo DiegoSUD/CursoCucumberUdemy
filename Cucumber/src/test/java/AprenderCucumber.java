7bc9a9a6578a2a4dfe247a1f3c94d20373b1529d
Diego Pereira Barroso <diego.barroso@rsinet.com.br>
2019-10-31 17:54:43 -0300

    estudando 31-10

---
+++ Cucumber/src/test/java/AprenderCucumber.java
@@ -0,0 +1,22 @@
+import cucumber.api.java.en.Given;
+import cucumber.api.java.en.Then;
+import cucumber.api.java.en.When;
+
+public class AprenderCucumber {
+	
+	@Given("^que criei o arquivo corretamente$")
+	public void que_criei_o_arquivo_corretamente() throws Throwable {
+		
+	}
+
+	@When("^executá-lo$")
+	public void executá_lo() throws Throwable {
+	   
+	}
+
+	@Then("^a exeucação deve rodar perfeitamente$")
+	public void a_exeucação_deve_rodar_perfeitamente() throws Throwable {
+	    
+	}
+
+}
---
+++ Cucumber/target/test-classes/aprender_cucumber.feature
@@ -0,0 +1,11 @@
+Feature: Aprender Cucumber
+		
+	Como um aluno 
+	Eu quero aprender a usar o Cucumber
+	Para que eu possoa automatizar criterios de aceitação
+	
+Scenario: Deve executar a expecificação
+
+	Given que criei o arquivo corretamente
+	When executá-lo
+	Then a exeucação deve rodar perfeitamente	
